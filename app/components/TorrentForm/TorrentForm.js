335d70aacfc478cd658c5191a59ac4eb0628224f
rllola <rllola80@gmail.com>
2019-02-10 19:36:54 +0100

    Added default link for magnet

--- app/components/TorrentForm/TorrentForm.js
+++ app/components/TorrentForm/TorrentForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { inject } from 'mobx-react'
-import { Header, Form, Button, Divider } from 'semantic-ui-react'
+import { Header, Form, Button, Divider, Input } from 'semantic-ui-react'
 
 @inject('site')
 class TorrentForm extends Component {
@@ -15,7 +15,7 @@ class TorrentForm extends Component {
   }
 
   submitTorrentURL = () => {
-    this.props.site.addTorrent(this.state.torrentURL).then(function (torrent) {
+    this.props.site.addTorrent(this.state.torrentURL).then((torrent) => {
       this.props.torrentAdded(torrent)
     }).catch(function (err) {
       console.error(err)
@@ -63,13 +63,19 @@ class TorrentForm extends Component {
     this.setState({torrentURL: event.target.value, latest: 'TorrentUrl'})
   }
 
+  clickSintelMagnet = (event) => {
+    event.preventDefault()
+
+    this.setState({torrentURL: event.target.href})
+  }
 
   render () {
     return (
       <div style={{color: 'white', paddingBottom: '48px'}}>
         <br />
         <Header as='h2' inverted >Add torrent form</Header>
-        <span style={{ color: 'white' }}>You can find free torrent here so you can test : <a href='https://webtorrent.io/free-torrents'>webtorrent.io/free-torrents</a></span>
+        <span style={{ color: 'white' }}>You can find free torrent here so you can test : <a href='https://webtorrent.io/free-torrents'>webtorrent.io/free-torrents</a> or you can test with <a onClick={this.clickSintelMagnet} href='magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'>Sintel (click here)</a>
+        </span>
         <br />
         <br/>
         <Form inverted onSubmit={this.onSubmit}>
